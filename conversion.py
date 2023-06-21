58ddc5a18ebb645a403fff0336fc8ad07910e4b5
komar1987 <83298900+komar1987@users.noreply.github.com>
2023-06-21 22:45:24 +0200

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -4,4 +4,8 @@ def celsius_to_fahrenheit(celsius):
 
 def celsius_to_kelvin(celcius):
     return celsius + 273.15
+
+def kelvin_to_fahrenheit(kelvin):
+    celsius = kelvin- 273.15
+    return celsius_to_fahrenheit(celsius)
     
